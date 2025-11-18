b3ca9b52f56b611d61d5fc0aaa89d76a903a5868
boming <boming0529@gmail.com>
2025-11-18 17:33:25 +0800

    [ADD] first time commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+
+def main():
+    print("--------------------------------------------------")
+    print("🚀 Hello from AWS CodeBuild (ARM64)!")
+    print(f"🐍 Python Version: {sys.version}")
+    print("✨ Built with 'uv' - The lightning fast package manager")
+    print("--------------------------------------------------")
+
+
+if __name__ == "__main__":
+    main()
